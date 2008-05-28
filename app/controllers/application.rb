f402a77f4a7118850e03f222cd5bda2c6b66b4e4
Leigh Caplan <lcaplan@onehub.com>
2008-05-27 20:22:53 -0700

    First commit

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '41147738c5392c3e7de7b08dd37c8879'
+end
